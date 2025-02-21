4e2fd02a3723188742aaa00ab89cd01d8ac2fc61
nima007 <nima.majmaa1@gmail.com>
2025-02-21 17:23:43 +0330

    start single blog

---
+++ pages/blogs/[slug].vue
@@ -0,0 +1,56 @@
+<template>
+    <main itemscope itemtype="https://schema.org/Article">
+        <h1 itemprop="headline" class="post-title">Blog Post Title</h1>
+        <div class="blog-data-row">
+            <div class="date-chips chips">
+                <img src="~/assets/images/cal.svg" alt="">
+                <span>10/11/1404</span>
+            </div>
+            <div class="spliter"></div>
+            <div class="chips">
+                <span>سوپاپ های لاسا</span>
+            </div>
+        </div>
+    </main>
+</template>
+<script setup>
+</script>
+<style scoped>
+main {
+    padding-top: calc(var(--menu-height) * 1.8);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.post-title{
+    font-size: 46px;
+}
+.blog-data-row {
+    width: fit-content;
+    display: flex;
+    gap: 12px;
+    color: #fff;
+}
+
+.blog-data-row .chips {
+    background: var(--dark-color);
+    height: 32px;
+    padding: 5px var(--gap);
+    display: flex;
+    gap: 8px;
+    border-radius: 16px;
+    align-items: center;
+}
+.blog-data-row .chips span{
+    margin: 0;
+    font-size: 14px;
+    height: 100%;
+    font-family: Kalameh-Light;
+    margin-top: 3px;
+}
+
+.blog-data-row .spliter{
+    width: 1px ;
+    background: var(--dark-color);
+}
+</style>
