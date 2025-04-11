9240d8dfb0a6fe4f94cf3c085a82251cd25d393c
nima007 <nima.majmaa1@gmail.com>
2025-04-11 04:33:31 +0330

    try to fix uboutus page  upadating operation .but not tested.

--- server/api/admin/pages/[pageName].get.js
+++ server/api/admin/pages/[pageName].get.js
@@ -3,6 +3,6 @@ import pageModel from "~/server/db/models/page.model";
 export default defineEventHandler(async event=>{
     const pageName = getRouterParam(event,'pageName');
     if(!pageName) return createError({statusCode:400});
-    const findedPage = await pageModel.findOne({pageName});
+    const findedPage = await pageModel.findOne({pageName}).select('-_v');
     return findedPage
 })
