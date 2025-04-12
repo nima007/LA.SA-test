65f38cc8fdc09338dae6c7821cdb77d25407a1be
nima007 <nima.majmaa1@gmail.com>
2025-04-13 03:03:58 +0330

    fix updating about us pgae content with remove __v from recovering data

--- server/api/admin/pages/[pageName].get.js
+++ server/api/admin/pages/[pageName].get.js
@@ -3,6 +3,6 @@ import pageModel from "~/server/db/models/page.model";
 export default defineEventHandler(async event=>{
     const pageName = getRouterParam(event,'pageName');
     if(!pageName) return createError({statusCode:400});
-    const findedPage = await pageModel.findOne({pageName}).select('-_v');
+    const findedPage = await pageModel.findOne({pageName}).select('-__v');
     return findedPage
 })
